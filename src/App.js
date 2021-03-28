f1c8794febd2150c56420b2ec3b38441c106adf4
allexib <ooomegasib@gmail.com>
2021-03-28 21:25:47 +0400

    css-in-JS, emotion step 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { css } from '@emotion/css';
+import {css} from '@emotion/css';
+
+const bgColor = '#282c34';
 
 function App() {
-  return (
-    <div className={css`
-    text-align: center;
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-  `}>
-        <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+    return (
+        <div
+            className={css`
+            text-align: center;
+            background-color: ${bgColor};
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-size: calc(10px + 2vmin);
+            color: white;
+            `}
+            >
+            <img src={logo} className="App-logo" alt="logo"/>
+        </div>
+    );
 }
 
 export default App;
